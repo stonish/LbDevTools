16b2951433d378fa2b4790190897faef58386d3d
Marco Clemencic <marco.clemencic@cern.ch>
2018-03-20 00:01:43 +0100

    Fix to generic toolchain.cmake

--- LbDevTools/data/toolchain.cmake
+++ LbDevTools/data/toolchain.cmake
@@ -8,7 +8,7 @@
 # granted to it by virtue of its status as an Intergovernmental Organization  #
 # or submit itself to any jurisdiction.                                       #
 ###############################################################################
-cmake_minimum_required(VERSION 3)
+cmake_minimum_required(VERSION 3.0.2)
 
 # this check is needed because the toolchain is called when checking the
 # compiler (without the proper cache)
@@ -16,7 +16,7 @@ if(NOT CMAKE_SOURCE_DIR MATCHES "CMakeTmp")
 
   find_file(default_toolchain NAMES GaudiDefaultToolchain.cmake
             HINTS ${CMAKE_SOURCE_DIR}/cmake
-                  ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+                  ${CMAKE_CURRENT_LIST_DIR}/cmake)
   if(default_toolchain)
     include(${default_toolchain})
   else()

235c720c35e3cdc58fb38d9efb86f0dbf4234063
Marco Clemencic <marco.clemencic@cern.ch>
2018-03-21 00:25:52 +0100

    Python 2.6 is not supported

--- setup.py
+++ setup.py
@@ -96,14 +96,13 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
 
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
     # This field adds keywords for your project which will appear on the
     # project page. What does your project relate to?

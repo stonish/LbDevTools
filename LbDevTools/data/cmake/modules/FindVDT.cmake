7232777bbb06b74186efdb1cf1cf5b792be599a4
Marco Clemencic <marco.clemencic@cern.ch>
2021-02-08 09:18:08 +0100

    Add imported targets to FindVDT.cmake

--- LbDevTools/data/cmake/modules/FindVDT.cmake
+++ LbDevTools/data/cmake/modules/FindVDT.cmake
@@ -5,7 +5,11 @@
 #  VDT_INCLUDE_DIR
 #  VDT_INCLUDE_DIRS (not cached)
 #  VDT_LIBRARIES
-
+#
+# Imports:
+#
+#  VDT::vdt
+#
 find_path(VDT_INCLUDE_DIR vdt/vdtcore_common.h
           HINTS $ENV{VDT_ROOT_DIR}/include ${VDT_ROOT_DIR}/include)
 find_library(VDT_LIBRARIES NAMES vdt
@@ -19,3 +23,16 @@ INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(VDT DEFAULT_MSG VDT_INCLUDE_DIR VDT_LIBRARIES)
 
 mark_as_advanced(VDT_FOUND VDT_INCLUDE_DIR VDT_LIBRARIES)
+
+if(TARGET VDT::vdt)
+    return()
+endif()
+if(VDT_FOUND)
+  add_library(VDT::vdt INTERFACE IMPORTED)
+  target_include_directories(VDT::vdt SYSTEM INTERFACE "${VDT_INCLUDE_DIR}")
+  target_link_libraries(VDT::vdt INTERFACE "${VDT_LIBRARIES}")
+  # Display the imported target for the user to know
+  if(NOT ${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+    message(STATUS "  Import target: VDT::vdt")
+  endif()
+endif()

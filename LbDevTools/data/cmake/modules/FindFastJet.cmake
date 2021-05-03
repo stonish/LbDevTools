10cb1b7ff03d9ed4efacf0ee5ff71ac703565d52
Marco Clemencic <marco.clemencic@cern.ch>
2021-05-03 17:45:30 +0200

    Update FindFastJet.cmake

--- LbDevTools/data/cmake/modules/FindFastJet.cmake
+++ LbDevTools/data/cmake/modules/FindFastJet.cmake
@@ -1,3 +1,13 @@
+###############################################################################
+# (c) Copyright 2000-2021 CERN for the benefit of the LHCb Collaboration      #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
 # - Locate FastJet library
 # Defines:
 #
@@ -7,6 +17,10 @@
 #  FASTJET_LIBRARY
 #  FASTJET_LIBRARIES (not cached)
 #  FASTJET_LIBRARY_DIRS (not cached)
+#
+# Imports:
+#
+#  FastJet::FastJet
 
 find_path(FASTJET_INCLUDE_DIR fastjet/version.hh
           HINTS $ENV{FASTJET_ROOT_DIR}/include ${FASTJET_ROOT_DIR}/include)
@@ -24,3 +38,12 @@ mark_as_advanced(FASTJET_FOUND FASTJET_INCLUDE_DIR FASTJET_LIBRARY)
 set(FASTJET_INCLUDE_DIRS ${FASTJET_INCLUDE_DIR})
 set(FASTJET_LIBRARIES ${FASTJET_LIBRARY})
 get_filename_component(FASTJET_LIBRARY_DIRS ${FASTJET_LIBRARY} PATH)
+
+if(TARGET FastJet::FastJet)
+    return()
+endif()
+if(FastJet_FOUND)
+  add_library(FastJet::FastJet IMPORTED INTERFACE)
+  target_include_directories(FastJet::FastJet SYSTEM INTERFACE ${FastJet_INCLUDE_DIRS})
+  target_link_libraries(FastJet::FastJet INTERFACE ${FastJet_LIBRARY})
+endif()
